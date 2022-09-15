394f14e4a3e248a435c139ed107ebc66594e1d9a
Oluwashina <shinzbaba@gmail.com>
2022-09-15 14:52:17 +0100

    interraction added to banner section layout

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,20 @@
+
+ // initialize swiper
+ var swiper = new Swiper(".mySwiper", {
+    speed: 800,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 30,
+    mousewheelControl: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
 // toggle open and close of hamburger on mobile
 const openNav = document.querySelector('#open');
 const closeNav = document.querySelector('#close');
